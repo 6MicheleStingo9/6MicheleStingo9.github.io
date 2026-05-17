7b6b61b4535fa183f5eeb97de4a1df7f1ac5ad53
6MicheleStingo9 <153536646+6MicheleStingo9@users.noreply.github.com>
2026-05-17 15:02:45 +0000

    Deploying to gh-pages from @ 6MicheleStingo9/6MicheleStingo9.github.io@a8c3e32eaf6adf9add4388e048d1a0e86d52d436 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "6MicheleStingo9/portfolio",
+    "data-repo": "6MicheleStingo9/6MicheleStingo9.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
